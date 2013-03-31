3cb9892295e5c1f56b85d09296e24a08d1389d35
Kjetil Flovild-Midtlie <kfm@docstream.no>
2013-03-31 20:20:14 +0200

    emacs init mm

---
+++ emacs_init.el
@@ -0,0 +1,14 @@
+
+;; colors and div
+(custom-set-variables
+ '(ansi-color-names-vector
+	 ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (deeper-blue)))
+ '(inhibit-startup-screen t)
+ '(version-control t))
+
+
+(setq backup-directory-alist
+      `(("." . ,(concat user-emacs-directory "backup/")))
+      tramp-backup-directory-alist backup-directory-alist)
+
