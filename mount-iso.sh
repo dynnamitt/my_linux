217f9724ab0df8d620e726770cab4820d6d04a38
kjetil midtlie <kjetil.midtlie@gmail.com>
2021-05-05 12:26:55 +0200

    helper for iso mnt, use idt to convert .bin

---
+++ mount-iso.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo "no iso path given"
+  exit 1
+fi
+
+src=$(realpath $1)
+
+segm1=$(basename $(dirname $src))
+segm2=$(basename $src)
+
+mkdir -p ~/mnt/iso/$segm1/$segm2
+sudo mount -o loop $1 ~/mnt/iso/$segm1/$segm2
+
+ls -d ~/mnt/iso/$segm1/$segm2
